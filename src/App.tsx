7543b27a3c5d64b3d50968b99bee7b3a252a3603
teixeirarafa <teixeirarafa10@hotmail.com>
2022-05-27 14:12:06 -0300

    chore: inclui configurações do eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <h1>hello react</h1>
-  );
-}
+const App: React.FC = () => {
+  return <h1>hello react</h1>;
+};
 
 export default App;
