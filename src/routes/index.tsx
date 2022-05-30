79fda90d21db77d6a258c59922332f2930033448
teixeirarafa <teixeirarafa10@hotmail.com>
2022-05-30 16:10:47 -0300

    feat: adiciona nova rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "../pages/Detail";
-import Main from "../pages/Main";
+import Home from "../pages/Home";
+import SearchResult from "../pages/SearchResult";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/items/:id" element={<Detail />} />
+        <Route path="/items" element={<SearchResult />} />
       </Routes>
     </Router>
   );
