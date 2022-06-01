387f2bcfb220eedb393f24e614d9f5b2ddc2ccb8
teixeirarafa <teixeirarafa10@hotmail.com>
2022-05-31 21:23:13 -0300

    feat: adiciona método para formatação de valores

---
+++ web/src/utils/number-format-currency.ts
@@ -0,0 +1,19 @@
+interface NumberFormatCurrencyProps {
+  currency: string;
+  decimals: number;
+  value: number;
+}
+
+const numberFormatCurrency = ({
+  currency,
+  decimals,
+  value,
+}: NumberFormatCurrencyProps) => {
+  return new Intl.NumberFormat(currency, {
+    currency,
+    maximumFractionDigits: decimals,
+    minimumFractionDigits: decimals,
+  }).format(value);
+};
+
+export default numberFormatCurrency;
