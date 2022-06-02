1a68c527266c595b239a8fac774a3f1dc70a2b93
teixeirarafa <teixeirarafa10@hotmail.com>
2022-06-01 23:31:15 -0300

    feat: cria component Loader

---
+++ web/src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loader.style.scss";
+
+const Loader: React.FC = () => {
+  return (
+    <div className="container-loader">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default Loader;
